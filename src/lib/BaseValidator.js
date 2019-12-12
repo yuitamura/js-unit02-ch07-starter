4384c7fe0b7a84465414dc1d1131eaffb604784c
yuitamura <yuixtamura@gmail.com>
2019-12-12 10:44:26 +0100

    update errorResult

--- src/lib/BaseValidator.js
+++ src/lib/BaseValidator.js
@@ -4,6 +4,7 @@ export default class {
     this.typeName = typeName;
     this.type = type;
     this.result = {};
+    this._errorResult = this._errorResult.bind(this);
   }
   _cannotEmpty() {
     return new Promise((resolve, reject) => {
@@ -11,11 +12,21 @@ export default class {
         resolve(this);
       } else {
         reject({
-          success: false,
-          message: `${this.typeName}は必須です。`,
-          type: this.type
+          message: this._errorResult(`${this.typeName}は必須です。` ) // message = ... のイメージ
         })
       }
     });
+  }
+
+  _errorResult(message) {
+    if (message) {
+      return Promise.resolve(); // _cannotEmptyと連動するのでここではthis不要
+    } else {
+      return Promise.reject({ // ifのreturnとは別
+        success: false,
+        type: this.type,
+        message // 分割代入で省略。この関数宣言時点では中身がわからない、空にしておく
+      })
+    }
   }
 }
